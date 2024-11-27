d5845361aa18c0a81ddd9ebd6bb371f861b40db1
Amarjit Patel <amarjitpatel330@gmail.com>
2024-11-27 15:48:54 +0530

    ..

--- new/src/App.js
+++ new/src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HeroSection from './HeroSection/HeroSection';
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<HeroSection/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
